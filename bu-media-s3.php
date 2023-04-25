7aa90bc4aaa5a158f25909c9d7182d9df7ad5867
Jonathan Williams <tamlin@mac.com>
2023-04-25 12:07:03 -0400

    initial plugin framework

---
+++ bu-media-s3.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: bu-media-s3
+* Plugin URI: https://github.com/bu-ist/bu-media-s3
+* Description: A plugin for integrating S3 with WordPress
+* Version: 0.1
+* Author: Boston University
+* Author URI: https://developer.bu.edu/
+**/
+
