d9232829a7e788f5916a0b526cd48c2d8effe6e1
Jonathan Williams <tamlin@mac.com>
2023-05-10 11:47:51 -0400

    add table render to image size report

--- src/wp-cli-commands.php
+++ src/wp-cli-commands.php
@@ -4,6 +4,23 @@ namespace BU\Plugins\MediaS3;
 
 function get_custom_image_sizes() {
 	$image_sizes = wp_get_registered_image_subsizes();
-	echo json_encode( $image_sizes );
+
+	$image_size_table = array();
+
+	foreach ( $image_sizes as $name => $size ) {
+		$size_key    = $size['width'] . 'x' . $size['height'];
+		$crop_type   = gettype( $size['crop'] );
+		$crop_string = 'array' === $crop_type ? implode( ' ', $size['crop'] ) : wp_json_encode( $size['crop'], true );
+
+		$image_size_table[] = array(
+			'name'     => $name,
+			'width'    => $size['width'],
+			'height'   => $size['height'],
+			'crop'     => $crop_string,
+			'size-key' => $size_key,
+		);
+	}
+
+	\WP_CLI\Utils\format_items( 'table', $image_size_table, array( 'name', 'width', 'height', 'crop', 'size-key' ) );
 }
 \WP_CLI::add_command( 's3media get-custom-image-sizes', __NAMESPACE__ . '\\get_custom_image_sizes' );
